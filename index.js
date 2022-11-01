40ed340ca35908655691180111ac241b588e97c6
Bfunk54 <ben_fein@icloud.com>
2022-11-01 17:39:52 -0400

    added more questions

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const genMarkdown = require('generatorMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      message: 'What is the name of your project?',
-      name: 'project',
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'project',
     },
     {
         type: 'input',
@@ -22,19 +22,34 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is the name of your project?',
-        name: 'projectName',
+        message: 'Enter usage information for your project:',
+        name: 'usage',
     },
     {
-      type: 'input',
-      message: 'LinkedIn Url:',
-      name: 'linkedin',
+        type: 'input',
+        message: 'What is the full branch name of your project for contributions? ',
+        name: 'contributions',
+    },
+    {
+        type: 'input',
+        message: 'Enter testing instructions:',
+        name: 'testing',
+    },
+    {
+        type: 'list',
+        message: 'What license is your project under:',
+        name: 'license',
     },
     {
       type: 'input',
       message: 'Github Url:',
       name: 'github',
-    }
+    },
+    {
+        type: 'input',
+        message: 'Email Address:',
+        name: 'email',
+    },
 ];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
