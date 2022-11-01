c9c24d90911e80edd0aedc95bdf8c5cb4e6adbb2
Bfunk54 <ben_fein@icloud.com>
2022-11-01 17:20:59 -0400

    required the markdowngen package and fixed the comment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// TODO: Include packages needed for this application
+// Require the packages needed for this project
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genMarkdown = require('generatorMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
