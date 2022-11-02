b0e30aeaa6949d33e275d801abd8737ad2eeda63
Bfunk54 <ben_fein@icloud.com>
2022-11-02 09:28:38 -0400

    created the render license link section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
- 
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT License' :
+      return '[MIT License](https://opensource.org/licenses/MIT)'
+
+    case 'GNU' :
+      return '[GNU](https://www.gnu.org/licenses/)'
+
+    case 'Creative Commons' :
+      return '[Creative Commons](https://creativecommons.org/)'
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,7 +25,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
+  return `${renderLicenseBadge(data.license)}
 
 # ${data.project}
 
@@ -32,7 +43,7 @@ ${data.usage}
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 ##License
-${data.license}
+${renderLicenseLink(data.license)}
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 ##Contributing
